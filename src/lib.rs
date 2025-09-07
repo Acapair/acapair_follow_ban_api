eb4cf85f2df1231c71a5b977f2b6c25a860f150c
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2025-09-07 13:10:00 +0300

    test: :white_check_mark: add tests refactor: :recycle: abstractions chore: :truck: new file structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
+use serde::{Deserialize, Serialize};
+use surrealdb::sql::{Id, Thing};
+
 pub mod db;
 pub mod routing;
+pub mod tests;
 
 #[derive(Debug, Clone)]
 pub struct AppState {}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Channel {
+    pub id: Option<Thing>,
+    pub username: String,
+    pub follower_list: Vec<Id>,
+    pub banned_list: Vec<Id>,
+    pub followed_list: Vec<Id>,
+    pub banned_from_list: Vec<Id>,
+}
